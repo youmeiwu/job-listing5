ed3dab15ee55be396b9dbee03627e71f8913ec04
sarah <smile-wu@hotmail.com>
2017-08-07 22:29:24 +0800

    can upload and submit resume

---
+++ app/controllers/resumes_controller.rb
@@ -0,0 +1,27 @@
+class ResumesController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @job = Job.find(params[:job_id])
+    @resume = Resume.new
+  end
+
+  def create
+    @job = Job.find(params[:job_id])
+    @resume = Resume.new(resume_params)
+    @resume.user = current_user
+    @resume.job = @job
+    if @resume.save
+      flash[:notice] = "简历投递成功。"
+      redirect_to jobs_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def resume_params
+    params.require(:resume).permit(:content, :attachment)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170806135031) do
+ActiveRecord::Schema.define(version: 20170807141030) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 20170806135031) do
     t.integer  "wage_upper_bound"
     t.string   "contact_email"
     t.boolean  "is_hidden",        default: true
+  end
+
+  create_table "resumes", force: :cascade do |t|
+    t.text     "content"
+    t.string   "attachment"
+    t.integer  "user_id"
+    t.integer  "job_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
