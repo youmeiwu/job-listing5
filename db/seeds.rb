f18ce842ac2abf339e57b5d835886a24e8d1559d
sarah <smile-wu@hotmail.com>
2017-08-08 14:45:30 +0800

    seed jobs and admin acount

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "This seed will automatically create one admin acount, 10 public jobs and 10 hidden jobs"
+
+create_account = User.create([email: "a@gmail.com", password: "123456", password_confirmation: "123456", is_admin: "true"])
+puts "admin account created"
+
+create_jobs = for i in 1..10 do
+  Job.create!([title: "Job no.#{i}", description: "no.#{i} public job generated with seed", wage_upper_bound: rand(50..90)*1000, wage_lower_bound: rand(1..49)*1000, is_hidden: "false"])
+end
+puts "10 public jobs created"
+
+create_jobs = for i in 1..10 do
+  Job.create!([title: "Job no.#{i+10}", description: "no.#{i+10} hidden job generated with seed", wage_upper_bound: rand(50..90)*1000, wage_lower_bound: rand(1..49)*1000, is_hidden: "true"])
+end
+puts "10 hidden jobs created"
